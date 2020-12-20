98912d151a80cf4fd06d1c7ddf80b0de879f0692
wykim111 <40811068+wykim111@users.noreply.github.com>
2020-12-21 00:11:57 +0900

    Update 1408 24.c

--- 구현/1408 24.c
+++ 구현/1408 24.c
@@ -2,18 +2,18 @@
 	1시간 = 3600 s
 	1분 = 60s
 	1초 = 1s
-
-
-	tc input) 
+	tc input)
 	23:59
 	00:00
-
 	이 경우 carry(올림수)에 대한 처리 필요.
 	23:59보다 00:00은 숫자상 작으므로  음수가 나오면,
-	시간은 +24, 분 + 60, 초+60으로 초 부터 연산해서
-	초를 연산하면서 분을 -1 감소시키고, 분은 연산하면서 시간을 -1 감소
-
-
+	시간은 +24, 분 + 60, 초+60으로 초 부터 연산
+	
+	초가 음수이면
+	초를 연산하면서 분을 -1 감소시키고,
+	
+	분이 음수이면
+	분을 연산하면서 시간을 -1 감소
 */
 
 #include<stdio.h>
@@ -24,7 +24,7 @@ char missionTime[10];
 long long convSecond(char* time)
 {
 	long long sumSecond = 0;
-	int h=0, m=0, s=0;
+	int h = 0, m = 0, s = 0;
 
 	//시간 환산
 	for (int i = 0; i < 2; i++)
@@ -50,14 +50,14 @@ long long convSecond(char* time)
 }
 int main()
 {
-	long long second1 = 0, second2 = 0,remainTime=0;
+	long long second1 = 0, second2 = 0, remainTime = 0;
 	int h, m, s;
 	scanf("%s", curTime);
 	scanf("%s", missionTime);
 
 	second1 = convSecond(curTime);
 	second2 = convSecond(missionTime);
-	
+
 	remainTime = second2 - second1;
 
 	h = remainTime / 3600;
@@ -82,7 +82,7 @@ int main()
 		h += 24;
 	}
 
-	printf("%02d:%02d:%02d\n", h,m,s);
+	printf("%02d:%02d:%02d\n", h, m, s);
 
 	return 0;
 }
