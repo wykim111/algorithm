af51e50dc5c18157f44b7e74fb819c69bc91e74f
wykim111 <40811068+wykim111@users.noreply.github.com>
2020-12-24 20:42:28 +0900

    Create 1735 분수 합.c

---
+++ 수학/1735 분수 합.c
@@ -0,0 +1,56 @@
+/*
+	분모가 같은 경우 
+		분자만 단순히 더함
+	분모가 다른 경우 
+		분모 A와 분자 A에 최대공약수로 분모 B를 나눈 몫을 곱한다
+		분모 B와 분자 B에 최대공약수로 분모 A를 나눈 몫을 곱한다
+	              
+*/
+
+#include<stdio.h>
+#include<math.h>
+
+int denominatorA, denominatorB, denominatorC;//분모 A,B
+int numeratorA, numeratorB, numeratorC;//분자 A,B
+
+int GCM(int a, int b)
+{
+	if (b == 0)
+		return a;
+
+	return GCM(b, a%b);
+}
+
+
+
+
+int main()
+{
+	int numGCM;
+	int denominatorTempA, denominatorTempB;//분모 A,B
+	int numeratorTempA, numeratorTempB;//분자 A,B
+	// input 분자, 분모
+	scanf("%d %d",&numeratorA,&denominatorA);
+	scanf("%d %d", &numeratorB, &denominatorB);
+
+	numGCM = GCM(denominatorA, denominatorB);
+
+	//분모 연산
+	denominatorTempA = denominatorA *(denominatorB / numGCM);
+	denominatorTempB = denominatorB * (denominatorA / numGCM);
+
+	//분자 연산
+	numeratorTempA = numeratorA * (denominatorB / numGCM);
+	numeratorTempB = numeratorB * (denominatorA / numGCM);
+
+	
+	denominatorC = denominatorTempA;//B도 연산하였기 때문에 denominatorB 가능
+	numeratorC = numeratorTempA + numeratorTempB;
+
+	//기약분수
+	numGCM = GCM(numeratorC, denominatorC);
+	printf("%d %d\n", numeratorC/numGCM, denominatorC/numGCM);
+
+
+	return 0;
+}
