1419d4e08c6aa961fc5438c97f22d5ed34d07478
wykim111 <40811068+wykim111@users.noreply.github.com>
2021-06-16 22:11:57 +0900

    Create 4779_칸토어집합.cpp

---
+++ 분할정복/4779_칸토어집합.cpp
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+
+char str[531441];
+int N;
+int len = 0;
+
+void blank(int left, int right)
+{
+	for (int i = left; i <= right; i++)
+	{
+		str[i] = ' ';
+
+	}
+
+}
+
+
+void go(int dep, int left, int right, int size)
+{
+	if (dep == 0)
+	{
+
+		return;
+	}
+
+	go(dep - 1, left, left + (size / 3) - 1, size / 3);
+	blank(left + ((size / 3)), left + ((size / 3)) + (size / 3) - 1);
+	go(dep - 1, left + ((size / 3) * 2), left + ((size / 3) * 2) + (size / 3) - 1, size / 3);
+
+}
+
+int main()
+{
+	while (scanf("%d", &N) != EOF)
+	{
+		memset(str, 0, sizeof(str));
+
+		if (N == 0)
+		{
+			printf("-\n");
+
+			continue;
+		}
+
+		for (int i = 0; i < (int)pow(3, N); i++)
+		{
+			str[i] = '-';
+		}
+
+		//printf("%s\n",str);
+
+		len = strlen(str);
+
+		go(N, 0, len - 1, len);
+
+		printf("%s\n", str);
+	}
+	return 0;
+}
