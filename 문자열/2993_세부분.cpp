35b295bc5ed1c834414569d1ffabe75ac158f2c5
wykim111 <40811068+wykim111@users.noreply.github.com>
2021-08-05 22:25:52 +0900

    Create 2993_세부분.cpp

---
+++ 문자열/2993_세부분.cpp
@@ -0,0 +1,44 @@
+/*
+	1) 이중 반복문으로 3부분으로 나눈다.
+	for (int i = 0; i < len - 2; i++)
+	{
+		for (int j = i + 1; j < len - 1; j++)
+		{
+
+		}
+	}
+	2) 오름차순 즉 제일 작은 문자열을  구한다.
+*/
+#include<iostream>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+string str;
+string ret_str;
+int main()
+{
+	int len = 0;
+
+	cin >> str;
+
+	len = str.length();
+	ret_str = 'z';
+	for (int i = 0; i < len - 2; i++)
+	{
+		for (int j = i + 1; j < len - 1; j++)
+		{
+			string temp = str;
+
+			reverse(temp.begin(),temp.begin()+(i+1));
+			reverse(temp.begin()+(i+1), temp.begin() + (j + 1));
+			reverse(temp.begin()+(j+1), temp.end());
+
+			ret_str = min(ret_str, temp);
+		}
+	}
+	cout << ret_str << '\n';
+
+	return 0;
+}
