7f29b5f6302ba4ff9ec07938ff0aa5bb06dd9693
wykim111 <40811068+wykim111@users.noreply.github.com>
2022-01-18 20:50:39 +0900

    Create perm_comb.c

---
+++ 이론/완전탐색/perm_comb.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+
+#define SIZE 3
+
+int arr[3] = { 1,2,3 };
+
+int set[10];
+int comb[10];
+
+void subset(int dep,int set_size, int curIdx)
+{
+	if (dep == SIZE)
+	{
+		for (int i = 0; i < set_size; i++)
+		{
+			printf("%d ", set[i]);
+		}
+		printf("\n");
+
+		return;
+	}
+	set[set_size] = arr[curIdx];
+	
+	subset(dep + 1,set_size+1, curIdx + 1);//pick
+
+	subset(dep + 1, set_size , curIdx + 1);// not pick
+}
+
+void permutation(int dep)
+{
+
+}
+
+void combination(int dep,int set_size, int curIdx,int pick)
+{
+	
+	if (pick == 2)
+	{
+		for (int i = 0; i < set_size; i++)
+		{
+			printf("%d ", comb[i]);
+		}
+		printf("\n");
+		
+		return;
+	}
+	/*
+		중요 : 모든 조합을 탐색해야 하는데 dep == SIZE가  함수 맨 위에 존재하면 배열의 마지막 데이터를 출력할 수 없음
+	*/
+	if (dep == SIZE)
+		return;
+
+	
+	
+	comb[set_size] = arr[curIdx];
+
+	combination(dep + 1, set_size + 1, curIdx + 1,pick+1);//pick
+
+	combination(dep + 1, set_size, curIdx + 1,pick);// not pick
+}
+
+
+int main()
+{
+	printf("부분집합\n");
+//	subset(0, 0, 0);
+
+	printf("조합\n");
+	combination(0, 0, 0, 0);
+
+	return 0;
+}
