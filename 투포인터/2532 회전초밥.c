00bd96f3a090fb0ec73f9c6c279af7236f80b765
wykim111 <40811068+wykim111@users.noreply.github.com>
2023-09-30 15:02:09 +0900

    Update 2532 회전초밥.c

--- 투포인터/2532 회전초밥.c
+++ 투포인터/2532 회전초밥.c
@@ -1,85 +1,110 @@
 /*
-벨트의 임의의 한 위치부터 k개의 접시를 연속해서 먹을 경우 할인된 정액 가격으로 제공한다.
- 1번 행사에 참가할 경우 이 쿠폰에 적혀진 종류의 초밥 하나를 추가로 무료로 제공한다. 
- 만약 이 번호에 적혀진 초밥이 현재 벨트 위에 없을 경우, 요리사가 새로 만들어 손님에게 제공한다.
+		<투 포인터 알고리즘>
 
- 
- - 투 포인터 활용
 
- */
++ 원래 회전 초밥은 손님이 마음대로 초밥을 고르고, 먹은 초밥만큼 식대를 계산하지만, 벨트의 임의의 한 위치부터 k개의 접시를 연속해서 먹을 경우 할인된 정액 가격으로 제공한다.
++ 각 고객에게 초밥의 종류 하나가 쓰인 쿠폰을 발행하고, 1번 행사에 참가할 경우 이 쿠폰에 적혀진 종류의 초밥 하나를 추가로 무료로 제공한다.
+  만약 이 번호에 적혀진 초밥이 현재 벨트 위에 없을 경우, 요리사가 새로 만들어 손님에게 제공한다.
++ 손님이 먹을 수 있는 초밥 가짓수의 최댓값을 구하는 프로그램을 작성하시오.
 
-#include<stdio.h>
 
-//접시의 수 N, 초밥의 가짓수 d, 연속해서 먹는 접시의 수 k, 쿠폰 번호 c
-int N, d, k, c; 
 
-int plate[30001];//접시의 수
-int sushi[3001];//초밥
-int cnt = 0, maxRet = -1;
+1. left지점을 출발점으로 연속해서 k개를 먹는다.
+2. 마지막 K번쨰 먹은 위치까지 right 지점으로 탐색한다.
+3. left와 right 사이에 먹은 초밥의 가짓수를 카운트
+4. 최댓값 업데이트
 
-int main()
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int N;//회전벨트에 놓인 접시의 수
+int d;//초밥의 가짓수
+int k;//연속해서 먹는 접시의 수
+int c;//쿠폰의 수
+
+int sushi_arr[30001];
+bool visit[30001];//초밥에 대한 중복 여부 체크
+int coupon = 0;
+
+
+int Modular = 0;
+int answer = 0;
+
+void input()
 {
-	scanf("%d %d %d %d", &N, &d, &k, &c);
+	cin >> N >> d >> k >> c;
+
+	Modular = N;
 
 	for (int i = 0; i < N; i++)
 	{
-		scanf("%d", &plate[i]);
+		cin >> sushi_arr[i];
 	}
+}
 
-	//투 포인터
-	for (int i = 0; i < k; i++)
+void solution()
+{
+
+
+	//투포인터 알고리즘
+	for (int left = 0; left < N; left++)
 	{
-		//처음 먹는 초밥
-		if (sushi[plate[i]] == 0)
+		int  duplicate_cnt = 0;//중복된 수 카운트
+		int coupon = 1;//쿠폰 초밥이 있는지 체크
+
+		memset(visit, false, sizeof(visit));
+
+		for (int right = 0; right < k; right++)
 		{
-			cnt++;
-		}
-		
-		sushi[plate[i]]++;
-	}
+			int idx = ((left + right) % Modular);
 
-	maxRet = cnt;
-
-	for (int i = 1; i < N; i++)
-	{
-
-		if (maxRet <= cnt)
-		{
-			//연속 4개 먹을 떄 c번을 먹어서 카운트 판단
-			//처음 c개를 먹는 경우
-			if (sushi[c] == 0)
+			if (visit[sushi_arr[idx]] == true)
 			{
-				maxRet++;
+				++duplicate_cnt;
 			}
 			else
 			{
-				maxRet = cnt;
+				visit[sushi_arr[idx]] = true;
 			}
+
+			if (sushi_arr[idx] == c)//쿠폰이 존재하는 경우
+			{
+				coupon = 0;
+			}
+
 		}
-
-
-		//현재에서 다음 칸 이동 시 현재에 먹은거 뺌 
-		sushi[plate[i-1]]--;
-
-		//위와 같이 뺄 때 2개이상 먹은 게 아닌 경우
-		//0개로 아에 먹지 않은 경우가 되므로
-		if (sushi[plate[i-1]] == 0)
+#if 0
+		for (int i = 0; i < sushi_vt.size(); i++)
 		{
-			cnt--;
+			cout << sushi_vt[i] << ' ';
 		}
-		int next = (i + k - 1) % N;
-		//다음칸으로 이동한 초밥이 처음 먹은 경우
-		if (sushi[plate[next]] == 0)
-		{
-			cnt++;
-		}
-		sushi[plate[next]]++;
+		cout << '\n';
+#endif
+
+		//초밥 먹은 종류 업데이트(같은 초밥이 중복된 경우 한 가지로 정의)
+		answer = max(answer, k - duplicate_cnt + coupon);
 
 	}
 
+	cout << answer << '\n';
+}
 
-	printf("%d\n", maxRet);
 
+int main()
+{
+	ios::sync_with_stdio(NULL);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	input();
+	solution();
 
 	return 0;
 }
